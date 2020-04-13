2a24f41cc67c60030b53edabba6053b453f78404
mido3801 <55100737+mido3801@users.noreply.github.com>
2020-04-13 12:50:23 -0600

    Delete pdfToText.py

--- pdfToText.py
+++
@@ -1,7 +0,0 @@
-from tika import parser
-
-with open('test3.txt','w') as outfile:
-    file = 'spring2020class_schedule.pdf'
-    file_data=parser.from_file(file)
-    text=file_data['content']
-    outfile.write(text)
